f357f80cee900c38e5e213aa8c8c18604ae9cfa2
syukri21 <syukrihsb148@gmail.com>
2018-12-18 23:02:54 +0700

    finish halaman home

--- src/components/footer/index-styles.js
+++ src/components/footer/index-styles.js
@@ -1,1 +1,25 @@
-export const styles = (theeme) => ({});
+export const styles = (theme) => ({
+	image: {
+		height: 150,
+		width: 100
+	},
+	footerLeft: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'flex-end',
+		paddingRight: 20,
+		background: '#101010'
+	},
+	footerRight: {
+		background: '#232426'
+	},
+	footerRightContent: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		padding: 20,
+		':& button': {
+			height: 50
+		}
+	}
+});
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,21 +3,77 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 import { styles } from './index-styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { loadCSS } from 'fg-loadcss/src/loadCSS';
+import Icon from '@material-ui/core/Icon';
 
 class Footer extends React.Component {
+	componentDidMount() {
+		loadCSS(
+			'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
+			document.querySelector('#insertion-point-jss')
+		);
+	}
 	render() {
+		const { classes } = this.props;
 		return (
-			<Grid container style={{ height: 300 }}>
-				<Grid
-					item
-					xs={12}
-					sm={3}
-					style={{ backgroundColor: '#141414', height: 300 }}
-				>
-					dsdsdssss
+			<Grid container>
+				<Grid item xs={12} sm={3} className={classes.footerLeft}>
+					<img src='assets/notflix.png' className={classes.image} alt='#' />
+					<Typography
+						style={{ color: 'white', transform: 'translateY(-50px)' }}
+					>
+						Netflox Inc &copy; 2018
+					</Typography>
 				</Grid>
-				<Grid item xs={12} sm={9} style={{ backgroundColor: '#232426' }}>
-					sdsdsdsd
+				<Grid item xs={12} sm={9} className={classes.footerRight}>
+					<Grid container style={{ height: '100%' }}>
+						<Grid item xs={12} sm={3} className={classes.footerRightContent}>
+							<Button variant='contained' color='primary'>
+								Menu
+							</Button>
+							<Button style={{ height: 50, color: 'white' }}>Home</Button>
+							<Button style={{ height: 50, color: 'white' }}>
+								Categories
+							</Button>
+						</Grid>
+						<Grid item xs={12} sm={3} className={classes.footerRightContent}>
+							<Button variant='contained' color='primary'>
+								GET APPS ON
+							</Button>
+							<Button
+								className='fab fa-google-play fa-2x'
+								style={{ height: 50, color: 'white' }}
+							/>
+							<Button style={{ height: 50, color: 'white' }} />
+						</Grid>
+						<Grid item xs={12} sm={3} className={classes.footerRightContent}>
+							<Button variant='contained' color='primary'>
+								Follow Us
+							</Button>
+							<Button
+								className='fab fa-facebook fa-2x'
+								style={{ height: 50, color: 'white' }}
+							/>
+							<Button
+								className='fab fa-instagram fa-2x'
+								style={{ height: 50, color: 'white' }}
+							/>
+						</Grid>
+						<Grid
+							item
+							xs={12}
+							sm={3}
+							className={classes.footerRightContent}
+							style={{ justifyContent: 'flex-start' }}
+						>
+							<Button variant='contained' color='secondary'>
+								SUBSCRIBE
+							</Button>
+						</Grid>
+					</Grid>
 				</Grid>
 			</Grid>
 		);
